3bce66a0bd0e4e8b66c228cbd967169ba6f04bd8
Maciej <maciek.widlak@gmail.com>
2026-01-28 13:48:03 +0100

    added initial text verification

--- app.py
+++ app.py
@@ -5,36 +5,75 @@ from openai import OpenAI
 import instructor
 from pydantic import BaseModel
 
+env = dotenv_values(".env")
+
+def get_openai_client():
+    return OpenAI(api_key=st.session_state["openai_api_key"])
+
+class ClassificationCategory(str, enum.Enum):
+    VALID = "valid"
+    NOT_VALID= "not_valid"
+
+def check_validity(description):
+    class ClassificationResult(BaseModel):
+        category: ClassificationCategory
+    instructor_openai_client = instructor.from_openai(get_openai_client())
+    res = instructor_openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        response_model=ClassificationResult,
+        messages=[
+            {
+                "role": "user",
+                "content": f"Sprawdź czy tekst zawiera KAŻDĄ z tych informacji: płeć && wiek && czas w bieganiu: {description}",
+            },
+        ],
+    )
+    return res.category
+     
 
 if "description" not in st.session_state:
     st.session_state["description"] = None
 
 if "is_description_valid" not in st.session_state:
-     st.session_state["is_description_valid"] not in st.session_state
+     st.session_state["is_description_valid"] = None
 
 if "response_json" not in st.session_state:
     st.session_state["response_json"] = None
 
+
+# OpenAI API key protection
+if not st.session_state.get("openai_api_key"):
+    if "OPENAI_API_KEY" in env:
+        st.session_state["openai_api_key"] = env["OPENAI_API_KEY"]
+
+    else:
+        st.info("Dodaj swój klucz Open API, aby korzystać z aplikacji")
+        st.page_link("https://platform.openai.com/account/api-keys", label="Zdobądź swój klucz tutaj", help= "Nie masz jeszcze klucza?", icon="🔑")
+        st.session_state["openai_api_key"] = st.text_input("klucz API", type="password")
+        if st.session_state["openai_api_key"]:
+            st.rerun()
+
+if not st.session_state.get("openai_api_key"):
+    st.stop()
 
 ### MAIN ###
 
 st.title("A jaki czas Ty możesz mieć na półmaratonie?")
 
 st.session_state["description"] = st.text_area(
-            "Powiedz nam o sobie. Jak szybko potrafisz przebiec 1km? 5km?", 
+            "Opowiedz nam o sobie. Podziel się osiągnięciami w biegu na długie dystanse (1km+)", 
             height=300, 
         )
 
 if st.session_state["description"]: 
         if st.button("Wyślij zapytanie"):
-            st.info("Zapytanie wysłane")  
             try:
-                transcript = create_transcription(open(st.session_state["audio_file_path"], "rb").read()) 
-            except AuthenticationError:
-                info_transcribe_placeholder.error("Invalid API key. Please check your OpenAI API key and try again (refresh site).")
-                st.stop()
+                if check_validity(st.session_state["description"]) == "valid":
+                    st.session_state["is_description_valid"] = True
+                    st.write("Dobre dane")
+                else:
+                    st.write("Dane nie są wystarczające do analizy. Najlepiej aby podany opis zawierał informację o wieku, płci i czasie na dowolny dłuszy dystans") 
             except Exception as e:
-                info_transcribe_placeholder.error(f"An error occurred: {str(e)}")
+                st.error(f"An error occurred: {str(e)}")
                 st.stop()
-            st.session_state["transcript"] = transcript
-            st.rerun()
+            
